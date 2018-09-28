6c872690f3285dbaefbac902e8fa6fe94506719e
Yanjiun <jiun113241@gmail.com>
2018-09-28 17:23:35 +0800

    Implement q_insert_tail and correct some bug
    some bug:
        I aware the including file in the queue.c.
            line 18 #include "harness.h"
        In the "harness.h".
    	line 58 #define malloc test_malloc
    	line 59 #define free test
        Our malloc() and free() function have benn replace.
        When we call the function, strdup(), in the <string.h>.
        The strdup() will also call malloc(),
        but it has not been replay to test_malloc.
        That is not the result designer want.

--- queue.c
+++ queue.c
@@ -29,8 +29,7 @@ queue_t *q_new()
     if (!(q = malloc(sizeof(queue_t)))) {
         return NULL;
     }
-    q->head = NULL;
-    q->tail = NULL;
+    q->head = q->tail = NULL;
     q->size = 0;
     return q;
 }
@@ -45,7 +44,7 @@ void q_free(queue_t *q)
     while (front) {
         tmp = front;
         front = front->next;
-        // free(tmp->value);
+        free(tmp->value);
         free(tmp);
     }
     /* Free queue structure */
@@ -70,7 +69,18 @@ bool q_insert_head(queue_t *q, char *s)
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     newh->next = q->head;
-    newh->value = strdup(s);
+    // newh->value = strdup(s);
+    int str_size = 0;
+    char *str_cp = s;
+    while (*str_cp != '\0') {
+        ++str_cp;
+        ++str_size;
+    }
+    if (!(str_cp = malloc(sizeof(char) * str_size))) {
+        free(newh);
+        return false;
+    }
+    newh->value = str_cp;
     q->head = newh;
     q->size++;
     if (q->size == 1) {
@@ -91,7 +101,30 @@ bool q_insert_tail(queue_t *q, char *s)
 {
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    if (!q)
+        return false;
+    list_ele_t *newt;
+    if (!(newt = malloc(sizeof(list_ele_t))))
+        return false;
+    newt->next = NULL;
+    int str_size = 0;
+    char *str_cp = s;
+    while (*str_cp != '\0') {
+        ++str_size;
+        ++str_cp;
+    }
+    if (!(str_cp = malloc(sizeof(char) * str_size))) {
+        free(newt);
+        return false;
+    }
+    newt->value = str_cp;
+    q->tail->next = newt;
+    q->tail = newt;
+    q->size++;
+    if (q->size == 1) {
+        q->head = newt;
+    }
+    return true;
 }
 
 /*
